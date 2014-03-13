a86630fef3dfff11e9fa1f4c6760598fb3670719
Frédéric K. <stradfred@gmail.com>
2014-03-13 15:09:24 +0100

    Update login.php
    Correct assets path

--- admin/login.php
+++ admin/login.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">	
 	<title>99ko - <?php echo lang('Login'); ?></title>	
-	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="css/foundation.min.css?v=5.0.3" media="all">
-	<link rel="stylesheet" href="css/99ko.min.css?v=1.0.2" />
+	<link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="assets/css/minified.css.php?v=5.2.0" media="all">
 	<?php showLinkTags(); ?>
 	<?php showScriptTags(); ?>
 	<?php eval(callHook('endAdminHead')); ?>	
