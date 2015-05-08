d8939fa24d9a99370f5e35954822891453133259
Frédéric K <stradfred@gmail.com>
2015-05-08 15:22:04 +0200

    replace h6 notification

--- common/show.class.php
+++ common/show.class.php
@@ -59,7 +59,7 @@ class show{
      	$data = '';
      	eval(callHook('startShowMsg'));
      	if($msg != '') $data = '<div data-alert class="alert-box '.$class[$type].' radius">
-     	                                <h6>'.nl2br($msg).'</h6><a href="#" class="close">&times;</a>
+     	                                <p>'.nl2br($msg).'</p><a href="#" class="close">&times;</a>
      	                        </div>';
       }
      	eval(callHook('endShowMsg'));
